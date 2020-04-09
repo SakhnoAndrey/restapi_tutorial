12a724fce7a4591b95ad3e7bb26b1811e9bfda19
SakhnoAndrey <Andrey.Sakhno@gmail.com>
2020-04-09 19:52:04 +0300

    Using generics classes

---
+++ generic_class/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GenericClassConfig(AppConfig):
+    name = "generic_class"
--- restapi_tutorial/urls.py
+++ restapi_tutorial/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("api/v2/snippets/", include("decorator_apiview.urls")),
     path("api/v3/snippets/", include("class_apiview.urls")),
     path("api/v4/snippets/", include("mixin_class.urls")),
+    path("api/v5/snippets/", include("generic_class.urls")),
 ]
